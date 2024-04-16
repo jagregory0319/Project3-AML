bfd9ca77c86349702a993b94b645f899d442e2e6
jagregory0319 <143538384+jagregory0319@users.noreply.github.com>
2024-04-16 14:11:43 +0000

    Add data cleaning and handling

--- model_prediction.py
+++ model_prediction.py
@@ -1,2 +1,8 @@
 # Project 3 - AML
 # Develop a predictive model to analyze and evaluate a dataset
+
+# Read and Clean Dataset
+df = pd.read_csv('cbb.csv')
+df = df.dropna()
+#Convert non-numeric values in column "POSTSEASON"
+convert = {"Champion" : 1, "2ND" : 2, "F4" : 3, "E8" : 4, "S16" : 5, "R32" : 6, "R64" : 7, "R68" : 8}
