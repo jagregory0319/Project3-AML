b30dc67e664ba9eebc1ea5c96b554f4de30c97b7
jagregory0319 <143538384+jagregory0319@users.noreply.github.com>
2024-04-16 14:13:41 +0000

    Add data cleaning and handling

--- model_prediction.py
+++ model_prediction.py
@@ -7,3 +7,4 @@ df = pd.read_csv('cbb.csv')
 df = df.dropna()
 #Convert non-numeric values in column "POSTSEASON"
 convert = {"Champion" : 1, "2ND" : 2, "F4" : 3, "E8" : 4, "S16" : 5, "R32" : 6, "R64" : 7, "R68" : 8}
+df["POSTSEASON"] = df['POSTSEASON'].map(convert)
