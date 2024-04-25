6c25f136a037cb21672b88982d8a839e70e9295b
Riley Jarosz <122238616+rileyjarosz@users.noreply.github.com>
2024-04-25 17:33:15 +0000

    Update C

--- model_prediction.py
+++ model_prediction.py
@@ -17,7 +17,7 @@ df = df[df['YEAR'] != 2023]
 # Convert non-numeric values in column "POSTSEASON"
 convert = {"Champion" : 1, "2ND" : 2, "F4" : 3, "E8" : 4, "S16" : 5, "R32" : 6, "R64" : 7, "R68" : 8}
 df['POSTSEASON'] = df['POSTSEASON'].map(convert)
-df['POSTSEASON'].fillna(9, inplace=True)
+df.fillna({'POSTSEASON': 9}, inplace = True)
 
 # Split dataframe into testing and training data
 X = df[['G','W','ADJOE','ADJDE','BARTHAG','EFG_O','EFG_D','TOR','TORD','ORB','DRB','FTR','FTRD','2P_O','2P_D','3P_O',
@@ -27,7 +27,7 @@ X_train, X_test, y_train, y_test = \
     train_test_split(X, y, test_size=.3, stratify=y)
 
 # Train and fit SVM model        
-svm_m = svm.SVC(kernel='rbf', C=10, probability = True, class_weight="balanced", gamma='scale', tol=0.0001)
+svm_m = svm.SVC(kernel='rbf', C=100, probability = True, class_weight="balanced", gamma='scale', tol=0.0001)
 svm_model = Pipeline ([
     ('scaler', StandardScaler()),
     ('svm', svm_m)
@@ -43,12 +43,10 @@ cm = confusion_matrix(y_test, y_pred)
 print("Confusion Matrix:")
 print(cm)
 
-print("Done!")
-
 # Read and convert test data
 s23_df = pd.read_csv('cbb23.csv')
 s23_df['POSTSEASON'] = s23_df['POSTSEASON'].map(convert)
-s23_df['POSTSEASON'].fillna(9, inplace=True)
+s23_df.fillna({'POSTSEASON': 9}, inplace = True)
 X_new = s23_df[['G','W','ADJOE','ADJDE','BARTHAG','EFG_O','EFG_D','TOR','TORD','ORB','DRB','FTR','FTRD','2P_O','2P_D','3P_O',
        '3P_D','ADJ_T']]
 y_new = s23_df['POSTSEASON']
@@ -60,11 +58,10 @@ teams = s23_df.loc[y_new.index, 'TEAM']
 svm_predictions_df = pd.DataFrame({'TEAM': teams, 'Predictions': svm_probs})
 svm_predictions_df['Predictions'] = svm_predictions_df['Predictions']
 svm_predictions_df.to_csv('svm_pred.csv', mode='w', index=False)
-print("Done!")
 
 # Calculate accuracy and confusion matrix of test
 accuracy_final_predictions = accuracy_score(y_new, svm_predictions)
-print("Accuracy of final predictions:", accuracy_final_predictions)
+print("\nAccuracy of 2023 predictions:", accuracy_final_predictions)
 cm_final_predictions = confusion_matrix(y_new, svm_predictions)
-print("Confusion Matrix for final predictions:")
+print("Confusion Matrix for 2023 predictions:")
 print(cm_final_predictions)
